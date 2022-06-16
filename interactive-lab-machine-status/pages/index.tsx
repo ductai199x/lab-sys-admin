03dc4e986e2b071db651706cb9c8fa7460749c73
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2022-06-16 01:43:10 -0400

    finally finished building an interactive web page for the lab statuses

---
+++ interactive-lab-machine-status/pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Welcome to MISL Lab</title>
+        <meta name="description" content="MISL Lab Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://misl.ece.drexel.edu/">MISL Lab @ Drexel University</a>
+        </h1>
+
+        <div className={styles.grid}>
+          <a href="lab-status" className={styles.card}>
+            <h2>Lab Machine Status</h2>
+            <p>A page detailing the statuses of the lab's machines</p>
+          </a>
+
+         
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
---
+++ interactive-lab-machine-status/pages/lab-status.tsx
@@ -0,0 +1,262 @@
+import fs from "fs";
+import YAML from "yaml";
+
+import { useEffect, useState } from "react";
+import { Button, Card, Collapse, Intent, Text } from "@blueprintjs/core";
+import type { NextPage } from "next";
+
+interface GpuStats {
+  [name: string]: {
+    gpu_mem_usage: {
+      total: string;
+      used: string;
+      free: string;
+    };
+    gpu_freq: {
+      sm_clock: string;
+      mem_clock: string;
+    };
+    gpu_temp: string;
+    gpu_util: {
+      gpu_util: string;
+      memory_util: string;
+      encoder_util: string;
+      decoder_util: string;
+    };
+    gpu_processes: string;
+    gpu_process_info: string;
+    nvidia_driver: string;
+    cuda_versions: string;
+    cudnn_versions: string;
+  };
+}
+
+interface AllGpuStats {
+  data: {
+    [name: string]: GpuStats[];
+  };
+}
+
+interface GpuProc {
+  pid: number;
+  name: string;
+  compute_instance_id: string | number;
+  gpu_instance_id: string | number;
+  type: string;
+  used_memory: string;
+  user: string;
+  start_time: string;
+  elapsed_time: string;
+  command: string;
+}
+
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (event: any) => {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
+const useEscape = (onEscape: any, onEscapeParams: any) => {
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      if (event.code === "Escape" && typeof onEscape === "function") onEscape(onEscapeParams);
+    });
+
+    return () => {
+      window.removeEventListener("keydown", (event) => {});
+    };
+  }, []);
+};
+
+const LabStatusPage: NextPage<AllGpuStats> = ({ data }) => {
+  const [mounted, setMounted] = useState(false);
+  const [gpu_running_processes, setGpuRunningProcesses] = useState<GpuProc[]>([]);
+  const [show_gpu_procs, setShowGpuProcs] = useState(false);
+  const [show_gpu_procs_spawn_pos, setShowGpuProcsSpawnPos] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const cursor_pos = useMousePosition();
+  const escape = useEscape(setShowGpuProcs, false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return <div></div>;
+
+  const extractInt = (strVal: string) => parseInt(strVal.split(" ")[0]);
+
+  const isGpuAvailable = (gpu_name: string, memory_util: string) => {
+    const availabilityClass =
+      gpu_name == "UNAVAILABLE" ? "unavailable" : extractInt(memory_util) > 500 ? "inuse" : "available";
+    return availabilityClass;
+  };
+
+  const getGpuRunningProcesses = (host_name: string, gpu_idx: number, gpu_name: string) => {
+    const gpu_proc_str = data[host_name][gpu_idx][gpu_name].gpu_processes;
+    const gpu_proc_info_str = data[host_name][gpu_idx][gpu_name].gpu_process_info;
+    let processes: Array<any> = JSON.parse(gpu_proc_str.replaceAll("'", '"')).process_info;
+    let processes_info = JSON.parse(gpu_proc_info_str.replaceAll("'", '"'));
+
+    let gpu_proc_list: GpuProc[] = [];
+    processes.map((proc) => {
+      let gpu_proc: any = {};
+      gpu_proc.pid = proc.pid;
+      gpu_proc.name = proc.process_name;
+      gpu_proc.compute_instance_id = proc.compute_instance_id;
+      gpu_proc.gpu_instance_id = proc.gpu_instance_id;
+      gpu_proc.type = proc.type;
+      gpu_proc.used_memory = proc.used_memory;
+
+      let proc_info: string[] = processes_info[gpu_proc.pid!].split(",");
+      proc_info = proc_info.map((s) => s.trim());
+
+      gpu_proc.user = proc_info[0];
+      gpu_proc.start_time = proc_info[1];
+      gpu_proc.elapsed_time = proc_info[2];
+      gpu_proc.command = proc_info[3];
+
+      gpu_proc_list.push(gpu_proc as GpuProc);
+    });
+    setGpuRunningProcesses(gpu_proc_list);
+    setShowGpuProcs(true);
+    setShowGpuProcsSpawnPos(cursor_pos);
+  };
+
+  const getProcTable = (gpu_running_processes: GpuProc[]) => (
+    <table className="bp4-html-table-condensed">
+      <thead>
+        <tr>
+          <th>pid</th>
+          <th>name</th>
+          <th>used_memory</th>
+          <th>user</th>
+          <th>start_time</th>
+          <th>elapsed_time</th>
+          <th>command</th>
+        </tr>
+      </thead>
+      <tbody>
+        {gpu_running_processes.map((proc) => (
+          <tr key={proc.pid}>
+            <td>{proc.pid}</td>
+            <td>{proc.name}</td>
+            <td>{proc.used_memory}</td>
+            <td>{proc.user}</td>
+            <td>{proc.start_time}</td>
+            <td>{proc.elapsed_time}</td>
+            <td>
+              <Text ellipsize={true} style={{ maxWidth: 300 }}>
+                {proc.command}
+              </Text>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  return (
+    <div>
+      <div
+        className="proc-info-wrapper"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          margin: 10,
+          zIndex: 10,
+          transform: `translate(${show_gpu_procs_spawn_pos.x + 5}px, ${show_gpu_procs_spawn_pos.y + 5}px)`,
+          backgroundColor: "rgba(174,249,248,1)",
+        }}
+      >
+        <Collapse isOpen={show_gpu_procs}>
+          {getProcTable(gpu_running_processes)}
+          <Button text="Close" fill={true} intent={Intent.WARNING} onClick={() => setShowGpuProcs(false)} />
+        </Collapse>
+      </div>
+
+      <table className="bp4-html-table bp4-interactive">
+        <thead>
+          <tr>
+            <th rowSpan={2}>machine</th>
+            <th rowSpan={2}>gpu_type</th>
+            <th colSpan={3}>gpu_mem_usage</th>
+            <th colSpan={2}>gpu_freq</th>
+            <th rowSpan={2}>gpu_temp</th>
+            <th colSpan={4}>gpu_util</th>
+            <th rowSpan={2}>nvidia_driver</th>
+            <th rowSpan={2}>cuda_versions</th>
+            <th rowSpan={2}>cudnn_versions</th>
+          </tr>
+          <tr>
+            <th>Total</th>
+            <th>Used</th>
+            <th>Free</th>
+            <th>SM</th>
+            <th>Mem</th>
+            <th>Total</th>
+            <th>Mem</th>
+            <th>Enc</th>
+            <th>Dec</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data).map((host_name) =>
+            data[host_name].map((host_obj, gpu_idx) =>
+              Object.keys(host_obj).map((gpu_name) => (
+                <tr
+                  key="`${host_name}_${gpu_name}`"
+                  className={isGpuAvailable(gpu_name, host_obj[gpu_name].gpu_mem_usage.used)}
+                  onClick={(e) => getGpuRunningProcesses(host_name, gpu_idx, gpu_name)}
+                >
+                  {gpu_idx == 0 ? <th rowSpan={data[host_name].length}>{host_name}</th> : null}
+                  <td>{gpu_name}</td>
+                  <td>{host_obj[gpu_name].gpu_mem_usage.total}</td>
+                  <td>{host_obj[gpu_name].gpu_mem_usage.used}</td>
+                  <td>{host_obj[gpu_name].gpu_mem_usage.free}</td>
+                  <td>{host_obj[gpu_name].gpu_freq.sm_clock}</td>
+                  <td>{host_obj[gpu_name].gpu_freq.mem_clock}</td>
+                  <td>{host_obj[gpu_name].gpu_temp}</td>
+                  <td>{host_obj[gpu_name].gpu_util.gpu_util}</td>
+                  <td>{host_obj[gpu_name].gpu_util.memory_util}</td>
+                  <td>{host_obj[gpu_name].gpu_util.encoder_util}</td>
+                  <td>{host_obj[gpu_name].gpu_util.decoder_util}</td>
+                  <td>{host_obj[gpu_name].nvidia_driver}</td>
+                  <td>{host_obj[gpu_name].cuda_versions}</td>
+                  <td>{host_obj[gpu_name].cudnn_versions}</td>
+                </tr>
+              ))
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const file = fs.readFileSync("/home/roggy/1-workdir/2-lab-sys-admin/lab_machine_status.yml", "utf8");
+  const data = await YAML.parse(file);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default LabStatusPage;
