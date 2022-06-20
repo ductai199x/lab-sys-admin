d1fe76d51de9bb5e0a24b938818ff4448526ec27
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2022-06-19 20:44:48 -0400

    minor fixes to the create_ansible_user bash script

--- run_create_ansible_user.sh
+++ run_create_ansible_user.sh
@@ -3,7 +3,7 @@
 target=$([ -z $1 ] && echo "labservers" || echo "$1")
 
 ansible-playbook -i hosts create-user-playbook.yml \
-  -e "user=ansible" \
-  -e "password={{ _ansible_password }}" \
+  -e "user={{ _ansbl_username }}" \
+  -e "password={{ _ansbl_password }}" \
   -e "target_hosts=$target" \
   --vault-password-file ./secret_key
