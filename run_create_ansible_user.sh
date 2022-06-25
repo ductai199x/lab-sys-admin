10da35d09233ac2f14ce23672055c9aee424dff3
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2022-06-24 22:15:24 -0400

    now allow edditing the group for user creation

--- run_create_ansible_user.sh
+++ run_create_ansible_user.sh
@@ -5,5 +5,6 @@ target=$([ -z $1 ] && echo "labservers" || echo "$1")
 ansible-playbook -i hosts create-user-playbook.yml \
   -e "user={{ _ansbl_username }}" \
   -e "password={{ _ansbl_password }}" \
+  -e "user_groups={{ ['sudo', 'misl'] | list }}" \
   -e "target_hosts=$target" \
   --vault-password-file ./secret_key
--- run_create_new_user.sh
+++ run_create_new_user.sh
@@ -5,5 +5,6 @@ target=$([ -z $3 ] && echo "labservers" || echo "$3")
 ansible-playbook -i hosts create-user-playbook.yml \
   -e "user=$1" \
   -e "password=$2" \
+  -e "user_groups={{ ['misl'] | list }}" \
   -e "target_hosts=$target" \
   --vault-password-file ./secret_key
