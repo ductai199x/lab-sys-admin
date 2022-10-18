90813be65622f4147864471d959b9950d0a7b640
Ansible Lab03 <ansible@lab03>
2022-10-18 11:59:27 -0400

    add a new role to disable unattended upgrades on cuda and cuda-drivers (nvidia-driver)

---
+++ run_unattended_upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+target=$([ -z $1 ] && echo "labservers" || echo "$1")
+
+ansible-playbook -i hosts main-playbook.yml \
+  -e "is_unattended_upgrade=True" \
+  -e "target_hosts=$target" \
+  --vault-password-file ./secret_key
