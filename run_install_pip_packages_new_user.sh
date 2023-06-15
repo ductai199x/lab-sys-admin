23b18a9d47a329703796d659e3d2961e5c0d2e11
Ansible Admin MISL Lab <ansible-admin@misl0.urcf.drexel.edu>
2023-06-15 14:24:27 -0400

    get everything working for the new transition to URCF

--- run_install_pip_packages_new_user.sh
+++ run_install_pip_packages_new_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 target=$([ -z $1 ] && echo "labservers" || echo "$1")
-user=$([ -z $2 ] && echo "admin" || echo "$2")
 
 ansible-playbook -i hosts main-playbook.yml \
   -e "is_install_pip_packages=True" \
   -e "target_hosts=$target" \
-  -e "user_choice=$user" \
+  -e "ansible_ssh_user=$2" \
+  -e "ansible_ssh_pass=$3" \
   --vault-password-file ./secret_key
--- run_unattended_upgrade.sh
+++ run_unattended_upgrade.sh
@@ -4,5 +4,6 @@ target=$([ -z $1 ] && echo "labservers" || echo "$1")
 
 ansible-playbook -i hosts main-playbook.yml \
   -e "is_unattended_upgrade=True" \
+  -e "user_choice=ansible" \
   -e "target_hosts=$target" \
   --vault-password-file ./secret_key
